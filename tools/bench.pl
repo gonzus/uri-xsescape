0686c42c2fb6cbe99566515a0f31aaff336b6a07
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-05-06 13:07:29 +0200

    Updated benchmark, easier to read results

--- tools/bench.pl
+++ tools/bench.pl
@@ -1,147 +1,82 @@
 #!/usr/bin/perl
-
 use strict;
 use warnings;
-use blib;
-use Dumbbench;
 
-use URI::Escape     ();
-use URI::Escape::XS ();
-use URI::XSEscape   ();
+use Benchmark qw/cmpthese/;
+use URI::Escape;
+use URI::Escape::XS;
+use URI::XSEscape;
 
-my $iterations = 1e4;
+my $orig_text = 'I said this: you / them ~ us & me _will_ "do-it" NOW!';
+my $orig_utf8 = 'http://www.google.co.jp/search?q=小飼弾';
+my $orig_code = 'I%20said%20this%3a%20you%20%2f%20them%20~%20us%20%26%20me%20_will_%20%22do-it%22%20NOW%21';
 
-exit main();
+my $orig_in = '!:/~&_-';
+my $orig_ni = '^a-zA-Z0-9';
+
+my @cases = (
+    uri_escape => sub {
+        cmpthese(
+            1000000 => {
+                'URI::Escape'     => sub {     URI::Escape::uri_escape($orig_text) },
+                'URI::Escape::XS' => sub { URI::Escape::XS::uri_escape($orig_text) },
+                'URI::XSEscape'   => sub {   URI::XSEscape::uri_escape($orig_text) },
+            },
+        );
+    },
+    uri_escape_in => sub {
+        cmpthese(
+            1000000 => {
+                'URI::Escape'     => sub {     URI::Escape::uri_escape($orig_text, $orig_in) },
+                'URI::Escape::XS' => sub { URI::Escape::XS::uri_escape($orig_text, $orig_in) },
+                'URI::XSEscape'   => sub {   URI::XSEscape::uri_escape($orig_text, $orig_in) },
+            },
+        );
+    },
+    uri_escape_not_in => sub {
+        cmpthese(
+            1000000 => {
+                'URI::Escape'     => sub {     URI::Escape::uri_escape($orig_text, $orig_ni) },
+                'URI::Escape::XS' => sub { URI::Escape::XS::uri_escape($orig_text, $orig_ni) },
+                'URI::XSEscape'   => sub {   URI::XSEscape::uri_escape($orig_text, $orig_ni) },
+            },
+        );
+    },
+    uri_escape_utf8 => sub {
+        cmpthese(
+            1000000 => {
+                'URI::Escape'     => sub {     URI::Escape::uri_escape_utf8($orig_utf8) },
+              # 'URI::Escape::XS' => sub { URI::Escape::XS::uri_escape_utf8($orig_utf8) },
+                'URI::XSEscape'   => sub {   URI::XSEscape::uri_escape_utf8($orig_utf8) },
+            },
+        );
+    },
+    uri_unescape => sub {
+        cmpthese(
+            1000000 => {
+                'URI::Escape'     => sub {     URI::Escape::uri_unescape($orig_code) },
+                'URI::XS::Escape' => sub { URI::Escape::XS::uri_unescape($orig_code) },
+                'URI::XSEscape'   => sub {   URI::XSEscape::uri_unescape($orig_code) },
+            },
+        );
+    },
+);
+
+exit main(@ARGV);
 
 sub main {
-    benchmark_escaping_ascii();
-    benchmark_escaping_utf8();
-    benchmark_unescaping();
+    my @argv = @_;
 
+    my $only = shift @argv;
+    my @versions = map { sprintf("%s %s", $_, $_->VERSION) } qw{URI::Escape URI::Escape::XS URI::XSEscape};
+    print join(' / ', @versions), "\n";
+
+    while (my ($name, $code) = splice(@cases, 0, 2)) {
+        next if $only && $only ne $name;
+        print "-- $name\n";
+        $code->();
+        print "\n";
+    }
     return 0;
 }
 
-sub benchmark_escaping_ascii {
-    my $bench = make_benchmark();
-    my $orig = 'I said this: you / them ~ us & me _will_ "do-it" NOW!';
-
-    $bench->add_instances(
-
-        Dumbbench::Instance::PerlSub->new(
-            name => 'URI::Escape',
-            code => sub {
-                for(1..$iterations){
-                    URI::Escape::uri_escape($orig);
-                }
-            },
-        ),
-
-        Dumbbench::Instance::PerlSub->new(
-            name => 'URI::Escape::XS',
-            code => sub {
-                for(1..$iterations){
-                    URI::Escape::XS::uri_escape($orig);
-                }
-            },
-        ),
-
-        Dumbbench::Instance::PerlSub->new(
-            name => 'URI::XSEscape',
-            code => sub {
-                for(1..$iterations){
-                    URI::XSEscape::uri_escape($orig);
-                }
-            },
-        ),
-    );
-
-    $bench->run;
-    $bench->report;
-    printf("== DONE ==========\n");
-}
-
-sub benchmark_escaping_utf8 {
-    my $bench = make_benchmark();
-    my $orig = 'http://www.google.co.jp/search?q=小飼弾';
-
-    $bench->add_instances(
-
-        Dumbbench::Instance::PerlSub->new(
-            name => 'URI::Escape',
-            code => sub {
-                for(1..$iterations){
-                    URI::Escape::uri_escape_utf8($orig);
-                }
-            },
-        ),
-
-#        Dumbbench::Instance::PerlSub->new(
-#            name => 'URI::Escape::XS',
-#            code => sub {
-#                for(1..$iterations){
-#                    URI::Escape::XS::uri_escape_utf8($orig);
-#                }
-#            },
-#        ),
-
-        Dumbbench::Instance::PerlSub->new(
-            name => 'URI::XSEscape',
-            code => sub {
-                for(1..$iterations){
-                    URI::XSEscape::uri_escape_utf8($orig);
-                }
-            },
-        ),
-    );
-
-    $bench->run;
-    $bench->report;
-    printf("== DONE ==========\n");
-}
-
-sub benchmark_unescaping {
-    my $bench = make_benchmark();
-    my $orig = 'I%20said%20this%3a%20you%20%2f%20them%20~%20us%20%26%20me%20_will_%20%22do-it%22%20NOW%21';
-
-    $bench->add_instances(
-
-        Dumbbench::Instance::PerlSub->new(
-            name => 'URI::Escape',
-            code => sub {
-                for(1..$iterations){
-                    URI::Escape::uri_unescape($orig);
-                }
-            },
-        ),
-
-        Dumbbench::Instance::PerlSub->new(
-            name => 'URI::Escape::XS',
-            code => sub {
-                for(1..$iterations){
-                    URI::Escape::XS::uri_unescape($orig);
-                }
-            },
-        ),
-
-        Dumbbench::Instance::PerlSub->new(
-            name => 'URI::XSEscape',
-            code => sub {
-                for(1..$iterations){
-                    URI::XSEscape::uri_unescape($orig);
-                }
-            },
-        ),
-    );
-
-    $bench->run;
-    $bench->report;
-    printf("== DONE ==========\n");
-}
-
-sub make_benchmark {
-    my $bench = Dumbbench->new(
-        target_rel_precision => 0.005,
-        initial_runs         => 20,
-    );
-    return $bench;
-}
