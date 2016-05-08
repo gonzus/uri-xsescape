a1c8d0c05b5909885e726a478c5cb576432b02bd
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-05-08 13:03:08 +0200

    Bump version to 0.000004

--- lib/URI/XSEscape.pm
+++ lib/URI/XSEscape.pm
@@ -6,7 +6,7 @@ use warnings;
 use XSLoader;
 use parent 'Exporter';
 
-our $VERSION = '0.000003';
+our $VERSION = '0.000004';
 XSLoader::load( 'URI::XSEscape', $VERSION );
 
 our @EXPORT_OK = qw{
@@ -38,7 +38,7 @@ URI::XSEscape - Quick & dirty URI escaping for Perl
 
 =head1 VERSION
 
-Version 0.000003
+Version 0.000004
 
 =head1 SYNOPSIS
 
