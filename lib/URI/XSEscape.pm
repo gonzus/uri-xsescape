b1ff7fcda966acd950ef7cbd6eac4eb708d5e86b
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-05-08 13:51:41 +0200

    Add docs and bump version to 0.000005

--- lib/URI/XSEscape.pm
+++ lib/URI/XSEscape.pm
@@ -6,7 +6,7 @@ use warnings;
 use XSLoader;
 use parent 'Exporter';
 
-our $VERSION = '0.000004';
+our $VERSION = '0.000005';
 XSLoader::load( 'URI::XSEscape', $VERSION );
 
 our @EXPORT_OK = qw{
@@ -24,6 +24,16 @@ sub uri_escape_utf8 {
     return uri_escape($text, $more);
 }
 
+# TODO: check if this is how we overwrite URI::Escape methods
+
+# eval {
+#     require URI::Escape;
+#
+#     *URI::Escape::uri_escape           = *URI::XSEscape::uri_escape;
+#     *URI::Escape::uri_escape_utf8      = *URI::XSEscape::uri_escape_utf8;
+#     *URI::Escape::uri_unescape         = *URI::XSEscape::uri_unescape;
+# };
+
 1;
 
 __END__
@@ -34,15 +44,77 @@ __END__
 
 =head1 NAME
 
-URI::XSEscape - Quick & dirty URI escaping for Perl
+URI::XSEscape - Fast XS URI-escaping library, replacing URI::Escape.
 
 =head1 VERSION
 
-Version 0.000004
+Version 0.000005
 
 =head1 SYNOPSIS
 
+    # load once
+    use URI::XSEscape;
+
+    # keep using URI::Escape as you wish
+
 =head1 DESCRIPTION
+
+By loading L<URI::XSEscape> anywhere, you replace any usage of
+L<URI::Escape> with a fast C implementation.
+
+You can continue to use L<URI::Escape> and any other module that
+depends on it just like you did before. It's just faster now.
+
+=head1 METHODS/ATTRIBUTES
+
+These match the API described in L<URI::Escape>.
+
+Please see those modules for documentation on what these methods and
+attributes are.
+
+=head2 uri_escape
+
+=head2 uri_escape_utf8
+
+=head2 uri_unescape
+
+=head1 BENCHMARKS
+
+For the common case, which is calling uri_escape with a single
+argument, and calling uri_unescape, URI::XSEscape runs between
+25 and 34 times faster than URI::Escape. The other cases are also
+faster, but the difference is not that noticeable.
+
+    $ perl -Iblib/lib -Iblib/arch tools/bench.pl
+    URI::Escape 3.31 / URI::Escape::XS 0.13 / URI::XSEscape 0.000004
+    -- uri_escape
+                        Rate     URI::Escape URI::Escape::XS   URI::XSEscape
+    URI::Escape       50839/s              --            -95%            -97%
+    URI::Escape::XS  961538/s           1791%              --            -47%
+    URI::XSEscape   1818182/s           3476%             89%              --
+
+    -- uri_escape_in
+                        Rate     URI::Escape URI::Escape::XS   URI::XSEscape
+    URI::Escape     107991/s              --            -16%            -83%
+    URI::Escape::XS 129032/s             19%              --            -80%
+    URI::XSEscape   641026/s            494%            397%              --
+
+    -- uri_escape_not_in
+                    Rate   URI::XSEscape     URI::Escape URI::Escape::XS
+    URI::XSEscape   39968/s              --            -11%            -37%
+    URI::Escape     45147/s             13%              --            -29%
+    URI::Escape::XS 63939/s             60%             42%              --
+
+    -- uri_escape_utf8
+                    Rate   URI::Escape URI::XSEscape
+    URI::Escape     40519/s            --          -96%
+    URI::XSEscape 1098901/s         2612%            --
+
+    -- uri_unescape
+                        Rate     URI::Escape URI::XS::Escape   URI::XSEscape
+    URI::Escape       74019/s              --            -93%            -96%
+    URI::XS::Escape 1086957/s           1368%              --            -43%
+    URI::XSEscape   1923077/s           2498%             77%              --
 
 =head1 AUTHORS
 
@@ -58,8 +130,10 @@ Version 0.000004
 
 =item * Brian Fraser
 
-=item * Sawyer X
+=item * Sawyer X C<< xsawyerx AT cpan DOT org >>
 
 =item * p5pclub
 
 =back
+
+__END__
