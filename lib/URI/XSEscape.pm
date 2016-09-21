6e03b2126b544dd1837b7d584db566b337900dd4
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-09-21 16:42:52 +0200

    Bump version

--- lib/URI/XSEscape.pm
+++ lib/URI/XSEscape.pm
@@ -6,7 +6,7 @@ use warnings;
 use XSLoader;
 use parent 'Exporter';
 
-our $VERSION = '0.000008';
+our $VERSION = '0.001000';
 XSLoader::load( 'URI::XSEscape', $VERSION );
 
 our @EXPORT_OK = qw{
@@ -52,7 +52,7 @@ L<URI::Escape>.
 
 =head1 VERSION
 
-Version 0.000008
+Version 0.001000
 
 =head1 SYNOPSIS
 
